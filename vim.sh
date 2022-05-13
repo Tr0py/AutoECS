9f545b01ef4d5f206ee4756acc0c42b74978a1dc
tropping <troppingz@gmail.com>
2022-05-13 14:52:56 -0500

    Disable vimrc <C-f> key binding

--- vim.sh
+++ vim.sh
@@ -4,6 +4,10 @@
 # using The Ultimate vimrc https://github.com/amix/vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+# Unset the annoying mapping of <C-d>  to CTRLP
+# comment out ctrlp key binding
+sed -i 's/let g:ctrlp_map/" let g:ctrlp_map/g' ~/.vim_runtime/vimrcs/plugins_config.vim
+
 # add cutomized vimrc
 cp ./vimrc ~/.vim_runtime/my_configs.vim
 # Use this to set up vim w/o cloning the repo
