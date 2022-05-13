29f791c31d192b37b360f313b6b9039607d36478
tropping <troppingz@gmail.com>
2022-05-13 14:10:59 -0500

    Add out-of-tree setup option for vim

--- vim.sh
+++ vim.sh
@@ -6,3 +6,5 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 # add cutomized vimrc
 cp ./vimrc ~/.vim_runtime/my_configs.vim
+# Use this to set up vim w/o cloning the repo
+# curl -o ~/.vim_runtime/my_configs.vim https://raw.githubusercontent.com/Tr0py/AutoECS-Environment-Config-Script/master/vimrc
