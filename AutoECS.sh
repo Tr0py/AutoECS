791d99a9302bc756a2e56e57dc8fbf32f19086d6
tropping <troppingz@gmail.com>
2022-07-04 15:56:21 -0500

    auto setup w/o git

---
+++ AutoECS.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/Tr0py/AutoECS-Environment-Config-Script.git ~/
+cd ~/AutoECS-Environment-Config-Script
+./config-all.sh
+
+# Clean up if you want
+# rm -rf ~/AutoECS-Environment-Config-Script
