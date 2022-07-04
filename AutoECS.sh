a86cba608087b95f8dc0cb1c84e9d2d9d7c133e9
tropping <troppingz@gmail.com>
2022-07-04 16:02:38 -0500

    Fix path.

--- AutoECS.sh
+++ AutoECS.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-git clone https://github.com/Tr0py/AutoECS-Environment-Config-Script.git ~/
-cd ~/AutoECS-Environment-Config-Script
+sudo apt install -y git
+git clone https://github.com/Tr0py/AutoECS-Environment-Config-Script.git
+cd AutoECS-Environment-Config-Script
 ./config-all.sh
 
 # Clean up if you want
-# rm -rf ~/AutoECS-Environment-Config-Script
+# rm -rf ./AutoECS-Environment-Config-Script
