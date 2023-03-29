c67b3d8d9b2780e2c9da5f39c1a8f6372f0f3c3f
tropping <troppingz@gmail.com>
2023-03-29 14:40:20 -0500

    tmux: add tmux resurrect.

--- tmux.sh
+++ tmux.sh
@@ -10,3 +10,7 @@ curl -o ~/.tmux.conf https://raw.githubusercontent.com/Tr0py/best.tmux.conf/mast
 # git clone https://github.com/gpakosz/.tmux.git ~/.tmux.git
 # ln -s -f ~/.tmux.git/.tmux.conf ~/.tmux.conf
 # cp ~/.tmux.git/.tmux.conf.local ~/.tmux.conf.local
+
+# Tmux resurrect
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/tmux-resurrect
+echo "run-shell ~/tmux-resurrect/resurrect.tmux" >> ~/.tmux.conf
