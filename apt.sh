e997a8e410a6778b9020244259b3551bd2f9c706
Ziyi Zhao <troppingz@gmail.com>
2024-06-10 09:13:09 -0700

    Add sshfs to apt install list.

--- apt.sh
+++ apt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install common softwares
-sudo apt install -y build-essential cmake curl tmux zsh silversearcher-ag vim git openssh-server tree net-tools
+sudo apt install -y build-essential cmake curl tmux zsh silversearcher-ag vim git openssh-server tree net-tools sshfs
